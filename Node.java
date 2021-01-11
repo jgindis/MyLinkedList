1d892be6d4e3b7725ddab4f32e746bceab0c5d78
jgindis <jgindis20@stuy.edu>
2021-01-11 14:15:15 -0500

    Made edits

--- Node.java
+++ Node.java
@@ -1,34 +1,26 @@
 public class Node {
   public Node(String value){
-     this.data = value;
-     this.next = null;
-     this.prev = null;
+     data = value;
+     next = null;
+     prev = null;
    }
    private String data;
    private Node next,prev;
 
    public String getData() {
-     return this.data;
+     return data;
    }
 
    public Node getNext() {
-     return this.next;
+     return next;
    }
 
    public Node getPrev() {
-     return this.prev;
+     return prev;
    }
 
    public void setData(String newvalue) {
-     this.data = newvalue;
-   }
-
-   public void setNext(Node newnext) {
-     this.next = newnext;
-   }
-
-   public void setPrev(Node newprev) {
-     this.prev = newprev;
+     data = newvalue;
    }
 
 
