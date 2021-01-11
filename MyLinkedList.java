08526183e9120f078c6db209369004d37470ca10
jgindis <jgindis20@stuy.edu>
2021-01-11 14:05:45 -0500

    Pushed for Mr. K

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -11,4 +11,104 @@ public class MyLinkedList {
     return size;
   }
 
+  private Node getNthNode(int n) {
+    Node current = this.start;
+    if ((n<0)||(n > this.size)) {
+      throw new IndexOutOfBoundsException("Index out of bounds");
+    }
+    for (int i = 0; i < n; i ++) {
+      current = current.getNext();
+      }
+   return current;
+  }
+
+  public boolean add(String value) {
+    if (this.size == 0) {
+      this.start = new Node(value);
+      this.end = this.start;
+      this.size = 1;
+      return true;
+    }
+    else {
+      Node newnode = new Node(value);
+      this.end.setNext(newnode);
+      newnode.setPrev(this.end);
+      this.end = newnode;
+      this.size ++;
+      return true;
+    }
+  }
+
+  public void add(int index, String value) {
+     Node newnode = new Node(value);
+     // EMPTY:
+    if (this.size == 0) {
+      this.start = newnode;
+      this.end = this.start;
+      this.size = 1;
+    }
+    // START:
+   else if (index == 0) {
+     newnode.setNext(this.start);
+     this.start.setPrev(newnode);
+     this.start = newnode;
+     this.size ++;
+   }
+    // END:
+   else if (index == this.size) {
+     this.end.setNext(newnode);
+     newnode.setPrev(this.end);
+     this.end = newnode;
+     this.size ++;
+   }
+   // MIDDLE:
+   else {
+     Node indexnode = getNthNode(index);
+     indexnode.getPrev().setNext(newnode);
+     newnode.setPrev(indexnode.getPrev());
+     newnode.setNext(indexnode);
+     indexnode.setPrev(newnode);
+     this.size ++;
+   }
+  }
+
+  public String get(int index){
+    return getNthNode(index).getData();
+  }
+
+  public String set(int index, String value){
+    Node oldnode = getNthNode(index);
+    getNthNode(index).setData(value);
+    return oldnode.getData();
+  }
+
+  public String toString() {
+    Node current = this.start;
+    String output = "[";
+    while (current != null) {
+      output += current.getData();
+      current = current.getNext();
+      if (current != null) {
+        output += ", ";
+      }
+    }
+    output += "]";
+    return output;
+  }
+
+  public String toStringReversed() {
+    Node current = this.end;
+    String output = "[";
+    while (current != null) {
+      output += current.getData();
+      current = current.getPrev();
+      if (current != null) {
+        output += ", ";
+      }
+    }
+    output += "]";
+    return output;
+  }
+ }
+
 }
